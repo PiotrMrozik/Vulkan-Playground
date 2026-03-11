44ce486094ef120b924133f7ba6e462f3a9de3c1
Piotr Mrozik <piter012@vp.pl>
2026-03-11 22:21:58 +0100

    Add GLLFW and Vulkan init

---
+++ Source/main.cpp
@@ -0,0 +1,22 @@
+#include "../Include/App.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  try {
+    HelloTriangleApplication app;
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+
+  return EXIT_SUCCESS;
+}
