b653f9cbb196a0c28a8d7352e78460e00b68dbde
Piotr Mrozik <piter012@vp.pl>
2026-03-12 12:03:41 +0100

    CHeck and choose physical device

--- Include/App.h
+++ Include/App.h
@@ -11,6 +11,7 @@ import vulkan_hpp;
 
 #include <cstdlib>
 #include <iostream>
+#include <map>
 #include <stdexcept>
 
 constexpr uint32_t WIDTH = 800;
@@ -43,7 +44,10 @@ private:
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
   }
 
-  void initVulkan() { createInstance(); }
+  void initVulkan() {
+    createInstance();
+    pickPhysicalDevice();
+  }
 
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
@@ -57,7 +61,6 @@ private:
   }
 
   void createInstance() {
-
     constexpr vk::ApplicationInfo appInfo{
         .pApplicationName = "Hello Triangle",
         .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
@@ -126,7 +129,68 @@ private:
     return extensions;
   }
 
+  void pickPhysicalDevice() {
+    std::vector<vk::raii::PhysicalDevice> physicalDevices =
+        instance.enumeratePhysicalDevices();
+    auto const devIter =
+        std::ranges::find_if(physicalDevices, [&](auto const &physicalDevice) {
+          return isDeviceSuitable(physicalDevice);
+        });
+    if (devIter == physicalDevices.end()) {
+      throw std::runtime_error("failed to find a suitable GPU!");
+    }
+    physicalDevice = *devIter;
+  }
+
+  bool isDeviceSuitable(vk::raii::PhysicalDevice const &physicalDevice) {
+
+    // Check if the physicalDevice supports the Vulkan 1.3 API version
+    bool supportsVulkan1_3 =
+        physicalDevice.getProperties().apiVersion >= vk::ApiVersion13;
+
+    // Check if any of the queue families support graphics operations
+    auto queueFamilies = physicalDevice.getQueueFamilyProperties();
+    bool supportsGraphics =
+        std::ranges::any_of(queueFamilies, [](auto const &qfp) {
+          return !!(qfp.queueFlags & vk::QueueFlagBits::eGraphics);
+        });
+
+    // Check if all required physicalDevice extensions are available
+    auto availableDeviceExtensions =
+        physicalDevice.enumerateDeviceExtensionProperties();
+    bool supportsAllRequiredExtensions = std::ranges::all_of(
+        requiredDeviceExtension,
+        [&availableDeviceExtensions](auto const &requiredDeviceExtension) {
+          return std::ranges::any_of(
+              availableDeviceExtensions,
+              [requiredDeviceExtension](auto const &availableDeviceExtension) {
+                return strcmp(availableDeviceExtension.extensionName,
+                              requiredDeviceExtension) == 0;
+              });
+        });
+
+    // Check if the physicalDevice supports the required features (dynamic
+    // rendering and extended dynamic state)
+    auto features = physicalDevice.template getFeatures2<
+        vk::PhysicalDeviceFeatures2, vk::PhysicalDeviceVulkan13Features,
+        vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>();
+    bool supportsRequiredFeatures =
+        features.template get<vk::PhysicalDeviceVulkan13Features>()
+            .dynamicRendering &&
+        features
+            .template get<vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>()
+            .extendedDynamicState;
+
+    // Return true if the physicalDevice meets all the criteria
+    return supportsVulkan1_3 && supportsGraphics &&
+           supportsAllRequiredExtensions && supportsRequiredFeatures;
+  }
+
   GLFWwindow *window;
   vk::raii::Context context;
   vk::raii::Instance instance = nullptr;
+  vk::raii::PhysicalDevice physicalDevice = nullptr;
+
+  std::vector<const char *> requiredDeviceExtension = {
+      vk::KHRSwapchainExtensionName};
 };
